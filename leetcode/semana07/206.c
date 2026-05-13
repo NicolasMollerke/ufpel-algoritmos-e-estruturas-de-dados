f07ce4ecfb1fcc641599885246d81a941050ee56
Nicolas Mollerke <nicolasmollerke@gmail.com>
2026-05-13 19:42:25 -0300

    atualiza o exercício 206

--- leetcode/semana07/206.c
+++ leetcode/semana07/206.c
@@ -1,10 +1,10 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+#include <stdio.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
 struct ListNode* reverseList(struct ListNode* head) {
     struct ListNode *ant = NULL;
     struct ListNode *atual = head;
